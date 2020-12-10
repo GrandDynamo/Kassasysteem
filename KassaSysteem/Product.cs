3454c1e3ffd2bf9655e513414928af756b40bf9e
Tim Gels <timgels1808@hotmail.com>
2020-12-10 19:59:47 +0100

    Added Product class

---
+++ KassaSysteem/Product.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KassaSysteem
+{
+    class Product
+    {
+        private int productId;
+        private string productName;
+        private double price;
+        private float sale;
+
+        /// <summary>
+        /// Retrieves id of the product.
+        /// </summary>
+        /// <returns></returns>
+        public int GetProductId()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Sets id of the product.
+        /// </summary>
+        /// <param name="id"></param>
+        public void SetProductId(int id)
+        {
+
+        }
+
+        /// <summary>
+        /// Retrieves name of the product.
+        /// </summary>
+        /// <returns></returns>
+        public string GetProductName()
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Sets name of the product.
+        /// </summary>
+        /// <param name="name"></param>
+        public void SetProductName(string name)
+        {
+
+        }
+
+        /// <summary>
+        /// Gets price of the product.
+        /// </summary>
+        /// <returns></returns>
+        public double GetPrice()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Sets price of product.
+        /// </summary>
+        /// <param name="price"></param>
+        public void SetPrice(double price)
+        {
+
+        }
+
+        /// <summary>
+        /// Gets amount of sale of the product.
+        /// </summary>
+        /// <returns></returns>
+        public double GetSale()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Sets amount of sale for the product.
+        /// </summary>
+        /// <param name="saleAmount"></param>
+        public void SetSale(double saleAmount)
+        {
+
+        }
+    }
+}
